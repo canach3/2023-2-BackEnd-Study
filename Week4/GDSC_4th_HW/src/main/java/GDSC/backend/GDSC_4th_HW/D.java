4435f96015d85ae4ad158d5875152153efb9029e
김정윤 <127319680+canach3@users.noreply.github.com>
2023-10-10 23:47:35 +0900

    Week4 과제 제출

---
+++ Week4/GDSC_4th_HW/src/main/java/GDSC/backend/GDSC_4th_HW/D.java
@@ -0,0 +1,14 @@
+package GDSC.backend.GDSC_4th_HW;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class D {
+    private C c;
+
+    public C getC() {
+        return c;
+    }
+}
